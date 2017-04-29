c87fe75806fb19951f21b186e72d1f0ccfd9b517
SamCheung <zhangbixiong@szxhinfo.com>
2017-04-29 12:37:12 +0800

    add camera test

--- src/AppApi/api/v1/index.php
+++ src/AppApi/api/v1/index.php
@@ -1509,6 +1509,20 @@ $app->delete(
 );
 
 $app->get(
+    '/camera/:did',
+    function($did) use ($app){
+       $response = $app->response;
+       $ch = curl_init();
+    　　curl_setopt($ch, CURLOPT_URL, "http://wx.zxing-tech.cn/api/v1/cgi-bin/video.pl?did="+$did);
+    　　curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    　　curl_setopt($ch, CURLOPT_HEADER, 0);
+    　　$output = curl_exec($ch);
+    　　curl_close($ch);
+       $response->setBody(rspData(0, $output));
+    }
+);
+
+$app->get(
     '/test',
     function() use ($app){
         echo "haha";
